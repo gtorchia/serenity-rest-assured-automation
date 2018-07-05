57225389b598332579a3ead4bd724b66730c3fdb
Giuseppe Torchia <giuseppe.torchia@gmail.com>
2018-07-05 15:09:32 +0100

    new class created to read data form CSV file. Added new direcotry "cvs" as repository of cirm data police

---
+++ src/main/java/utils/CSVReader.java
@@ -0,0 +1,46 @@
+package utils;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import userdefinedobject.SinglePoint;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+    private File in = new File("./csv/2017-12-north-yorkshire-stop-and-search.csv");
+
+    public void CSVReader() {
+    }
+
+    public List<SinglePoint> recordfromCSV() throws FileNotFoundException {
+
+
+        List<SinglePoint> recordinserted = new ArrayList<SinglePoint>();
+        FileReader readeCSV ;
+
+        try {
+            readeCSV = new FileReader(in);
+            System.out.println(in.getName());
+            Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(readeCSV);
+            for (CSVRecord record : records) {
+               s
+                recordinserted.add(new SinglePoint(record.get("Longitude") , record.get("Latitude"), record.get("Date").substring(0,10)));
+
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return recordinserted;
+    }
+
+
+
+}
+
+
